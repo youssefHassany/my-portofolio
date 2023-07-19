fd6bef0e1a2aa26f461f4bfb40da39c99ddd6cd3
unknown <youssefhassany22@gmail.com>
2023-07-19 18:51:45 +0300

    only one project remaining

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,7 @@ let burgerIcon = document.querySelector(".burger-icon");
 
 const handleMenu = () => {
     nav.classList.toggle("show-nav")
+    burgerIcon.classList.toggle("move-burger");
 }
 
 burgerIcon.addEventListener("click", handleMenu);
