9dc6194af8cbb166df8ddf005938daae95df9601
unknown <youssefhassany22@gmail.com>
2023-07-21 01:33:34 +0300

    final result

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,18 @@ const handleMenu = () => {
 }
 
 burgerIcon.addEventListener("click", handleMenu);
+
+nav.addEventListener('click', () => {
+    nav.classList.remove('show-nav') // to close nav menu on click
+})
+
+let year = document.querySelector("span");
+let date = new Date()
+year.innerHTML = date.getFullYear()
+
+let form = document.querySelector("form");
+let errorMsg = document.querySelector(".error-msg")
+
+form.onsubmit = () => {
+    errorMsg.innerHTML = "Something went wrong :("
+}
