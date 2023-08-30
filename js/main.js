32f57662375a610f9755f311c7e765aba5687d5c
unknown <youssefhassany22@gmail.com>
2023-08-30 22:03:24 +0300

    fixing contact and responsive bugs

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// for nav
+let nav = document.querySelector("nav");
+let burgerIcon = document.querySelector(".burger-icon");
+
+const handleMenu = () => {
+  nav.classList.toggle("show-nav");
+  burgerIcon.classList.toggle("move-burger");
+};
+
+burgerIcon.addEventListener("click", handleMenu);
+
+nav.addEventListener("click", () => {
+  nav.classList.remove("show-nav"); // to close nav menu on click
+});
+
+let year = document.querySelector("span");
+let date = new Date();
+year.innerHTML = date.getFullYear();
